5e3f924c10bb85b4861dee04373fbe894776412d
wniemiec <wiktoria.niemiec97@gmail.com>
2020-10-17 18:32:26 +0200

    Add new android project

---
+++ WloskieCoNiecoVol2/app/src/main/java/com/example/wloskieconiecovol2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wloskieconiecovol2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
